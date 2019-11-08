8f9c83dd001cb54a8ccbb2e3f21e7fe9020e2f71
Luke Oliff <luke@lukeoliff.com>
2019-11-08 12:41:22 +0000

    parse typeform data

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,16 +3,32 @@ var jwt = require('jsonwebtoken');
 var router = express.Router();
 require('dotenv').config();
 
-/* POST login webhook generates a magic link email to the provided email address */
-router.post('/login', function(req, res, next) {
+/* POST webhook generates a magic link email to the provided email address */
+router.post('/magiclink', function(req, res, next) {
+  let { answers } = req.body.form_response;
+
+  let answer = answers
+    .filter(function (answer) {
+      return process.env.FORM_FIELD_TYPE === answer.type
+        && answer.field.ref === process.env.FORM_FIELD_REF
+        ;
+    })
+    .map(function(answer) {
+      var field = answer.type;
+      return answer[field]
+    })
+    [0];
+
+  console.log(answer);
+
   // add user to users database if they're not there yet
+  
 
   // generate JWT token for the magic link
-  var token = jwt.sign({ email: req.body.email }, process.env.SECRET);
+  var token = jwt.sign({ email: answer }, process.env.SECRET);
 
   // email token in magic link
   console.log(token);
-  console.log(req.protocol, req.get('host'), req.originalUrl);
   console.log(req.protocol + '://' + req.get('host') + '/auth?token=' + token);
 
   // send webhook response
